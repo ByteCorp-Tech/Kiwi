2071328afd6d1c98f42c7ca48c995f210c74b4c8
shakeeb1998 <shakeebsiddiqui1998@gmail.com>
2025-12-09 19:10:31 +0500

    db check

--- tcms/settings/common.py
+++ tcms/settings/common.py
@@ -184,7 +184,7 @@ STORAGES = {
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = "/Kiwi/static/"
+STATIC_ROOT = "."
 
 
 AUTHENTICATION_BACKENDS = [
